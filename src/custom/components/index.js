19b8a984533b7e6699a2e9b5efcf10d554f951cc
Christophe <christophe.dame@camunda.com>
2024-11-18 13:36:30 +0100

    try $inject annotation to solve injection issue in didi

--- src/custom/components/index.js
+++ src/custom/components/index.js
@@ -2,12 +2,14 @@ import { Range, rangeType } from "./range";
 import { ApiSelect, apiSelectType } from "./api-select";
 
 class RangeField {
+  static $inject = ["formFields"];
   constructor(formFields) {
     formFields.register(rangeType, Range);
   }
 }
 
 class ApiSelectField {
+  static $inject = ["formFields"];
   constructor(formFields) {
     formFields.register(apiSelectType, ApiSelect);
   }
