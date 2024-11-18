de78f7f94b5c10219d2429e3f32d9ed722577cdf
Christophe <christophe.dame@camunda.com>
2024-11-18 16:10:11 +0100

    fix imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import '@bpmn-io/form-js/dist/assets/form-js.css';
 import '@bpmn-io/form-js/dist/assets/form-js-editor.css';
-import FormEditor from './form-js/form-editor';
-import Form from './form-js/form-viewer';
-import FormPlayground from './form-js/form-playground';
+import { Form, FormEditor, FormPlayground } from "@bpmn-io/form-js";
+import { RenderExtension, PropertiesPanelExtension } from "./custom";
 
 export const newForm = (options) => {
   options.additionalModules = [
